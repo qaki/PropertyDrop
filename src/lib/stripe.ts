c77f574b0be8d63364267286f228b99bad72047e
Qlirim Elezi <oneqaki.e.d@gmail.com>
2025-12-12 19:39:39 +0100

    Fix: Use Stripe API version matching SDK types (2025-11-17.clover)

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { env } from "~/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-07-30.basil",
+  apiVersion: "2025-11-17.clover",
   typescript: true,
 });
 
