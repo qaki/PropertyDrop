de16af320a7ff3732edd8d809fca4fb8f9359f79
Qlirim Elezi <oneqaki.e.d@gmail.com>
2025-12-12 19:35:07 +0100

    Update Stripe API version to 2025-07-30.basil

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { env } from "~/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2025-07-30.basil",
   typescript: true,
 });
 
