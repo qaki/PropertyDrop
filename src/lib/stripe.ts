d7a90a1e352efa230ed06973b6175bea67d1dda2
Qlirim Elezi <oneqaki.e.d@gmail.com>
2025-12-12 10:57:26 +0100

    Remove unused S3 and Stripe files - cleanup for Whop integration

--- src/lib/stripe.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-import { env } from "~/env";
-
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-12-18.acacia", // Latest stable version
-  typescript: true,
-});
-
